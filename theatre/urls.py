7c977b0dcb7be649a6f7a0220226e1bacbe2e49e
Oleksii Kiva <oleksii.kiva@gmail.com>
2024-05-02 17:04:37 +0300

    feat: create a route actors

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from theatre.views import GenreViewSet
+from theatre.views import GenreViewSet, ActorViewSet
 
 app_name = "theatre"
 
 router = routers.DefaultRouter()
 router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
 
 urlpatterns = [path("", include(router.urls)), ]
--- theatre/views.py
+++ theatre/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 
-from theatre.models import Genre
-from theatre.serializers import GenreSerializer
+from theatre.models import Genre, Actor
+from theatre.serializers import GenreSerializer, ActorSerializer
 
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects
     serializer_class = GenreSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects
+    serializer_class = ActorSerializer
