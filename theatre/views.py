56116bd38fedf2fbcbf9c0c5e122a1f036d8e0ed
Oleksii Kiva <oleksii.kiva@gmail.com>
2024-05-04 19:55:55 +0300

    feat: add filtering in PlayViewSet
    - by Play title
    - by Genres id
    - by Actors id

--- theatre/views.py
+++ theatre/views.py
@@ -44,6 +44,38 @@ class PlayViewSet(viewsets.ModelViewSet):
 
         return serializer_class
 
+    @staticmethod
+    def _params_to_ints(qs):
+        """
+        Converts a list of string IDs to a list of integers
+        """
+
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        """
+        Retrieve the plays with filters
+        """
+
+        title = self.request.query_params.get("title")
+        genres = self.request.query_params.get("genres")
+        actors = self.request.query_params.get("actors")
+
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if genres:
+            genres_ids = self._params_to_ints(genres)
+            queryset = queryset.filter(genres__id__in=genres_ids)
+
+        if actors:
+            actors_ids = self._params_to_ints(actors)
+            queryset = queryset.filter(actors__id__in=actors_ids)
+
+        return queryset.distinct()
+
 
 class TheatreHallViewSet(viewsets.ModelViewSet):
     queryset = TheatreHall.objects
