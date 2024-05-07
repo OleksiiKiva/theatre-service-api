4b00a13f8a933bb8eb305f564c0e7d0312a1ed67
Oleksii Kiva <oleksii.kiva@gmail.com>
2024-05-07 18:14:48 +0300

    feat: customize authorization
    - use email instead of the username field during registration
    - add route for login
    - add route for managing user

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import CreateUserView, ManageUserView, LoginUserView
 
 app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", LoginUserView.as_view(), name="token"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
